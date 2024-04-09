1e86c9ddaf8defb466ec6d4767f8c9e646ba6976
Hennessy Nguyen <anguyen137@fordham.edu>
2024-04-08 21:15:06 -0400

    add routes, model, controller for posts

---
+++ models/posts-model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    "avatar": {
+        type: String,
+        required: true
+    },
+    "title": {
+        type: String, 
+        required: true
+    },
+    "body": {
+        type: String, 
+        required: true
+    },
+    "isPrompt": {
+        type: Boolean, 
+        required: true
+    }
+    // "comments": {
+    //     type: Comment,
+    //     required: false
+    // },
+    // "reactions": {
+    //     type: Reaction, 
+    //     required: false
+    // }
+}, { timestamps: true })
+
+const postsModel = mongoose.model('Post', postSchema);
+module.exports = postsModel;
