b9577ba5a4e4ee326cd7898f9a5eb20b3ad223ed
Ethan Nguyen <gsthanhnguyen@gmail.com>
2024-04-10 15:21:43 -0700

    working in progress

--- app.js
+++ app.js
@@ -2,25 +2,34 @@ const mongoose = require('mongoose');
 const express = require('express')
 const cors = require('cors')
 
+// create routes
 const usersRouter = require('./routes/users-routes')
 const postsRouter = require('./routes/posts-routes')
+const commentsRouter = require('./routes/comments-routes')
+const reactionsRouter = require('./routes/reactions-routes')
 
-
+// create express app
 const app = express()
 
+// middleware: parse incoming JSON data
 app.use(cors({
     origin: "*"
 }))
 
 app.use(express.json())
 
+// test route to check if backend is connected
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Connected to Backend!' });
 });
 
+// use routes
 app.use('/api/users', usersRouter);
 app.use('/api/posts', postsRouter);
+app.use('/api/comments', commentsRouter)
+app.use('/api/reactions', reactionsRouter)
 
+// connect to MongoDB
 const MONGO_URI = 'mongodb+srv://peacepod:peacepod@cluster0.cxattxq.mongodb.net/' // process.env.MONGO_URI;
 mongoose.connect(MONGO_URI)
     .then(() => {
