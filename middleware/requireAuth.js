0ee341e0078144b81998990d7a47b3d7823281f0
Nam Nguyen <nguy1053@msu.edu>
2024-04-22 11:43:55 -0400

    Fix requireAuth

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -5,7 +5,7 @@ const requireAuth = async (req, res, next) => {
     const { authorization } = req.headers
 
     if (!authorization) {
-        res.status(401).json({error: "Authorization token required."})
+        return res.status(401).json({error: "Authorization token required."})
     }
 
     const token = authorization.split(' ')[1]
