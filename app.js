fb3506c01a88a8f36a0890626599f4b885d9bd2b
Jenny <dolientrang@gmail.com>
2024-04-23 19:13:13 -0400

    socketio

--- app.js
+++ app.js
@@ -80,21 +80,53 @@ var io = socket(server, {
   },
 });
 
-io.on('connection', (socket) => {
-
-    console.log('made socket connection', socket.id);
-
-    // Handle chat event
-    socket.on('chat', (data) => {
-        io.sockets.emit('chat', data);
+io.on("connection", (socket) => {
+    console.log("Connected to socket.io");
+    socket.on("setup", (userData) => {
+      socket.join(userData._id);
+      socket.emit("connected");
     });
+  
+    socket.on("join chat", (room) => {
+      socket.join(room);
+      console.log("User Joined Room: " + room);
+    });
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+  
+    socket.on("new message", (newMessageRecieved) => {
+      var chat = newMessageRecieved.chat;
+  
+      if (!chat.users) return console.log("chat.users not defined");
+  
+      chat.users.forEach((user) => {
+        if (user._id == newMessageRecieved.sender._id) return;
+  
+        socket.in(user._id).emit("message recieved", newMessageRecieved);
+      });
+    });
+  
+    socket.off("setup", () => {
+      console.log("USER DISCONNECTED");
+      socket.leave(userData._id);
+    });
+  });
 
-    // Handle typing event
-    socket.on('typing', (data) => {
-        socket.broadcast.emit('typing', data);
-    })
+// io.on('connection', (socket) => {
 
-});
+//     console.log('made socket connection', socket.id);
+
+//     // Handle chat event
+//     socket.on('chat', (data) => {
+//         io.sockets.emit('chat', data);
+//     });
+
+//     // Handle typing event
+//     socket.on('typing', (data) => {
+//         socket.broadcast.emit('typing', data);
+//     })
+
+// });
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
