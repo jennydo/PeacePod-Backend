0263e75bff575a5882fee5e6a8bd483fd4d482f9
Jenny <dolientrang@gmail.com>
2024-04-24 15:26:25 -0400

    fix

--- controllers/messages-controller.js
+++ controllers/messages-controller.js
@@ -7,7 +7,7 @@ const Chat = require("../models/chats-model");
 const allMessages = asyncHandler(async (req, res) => {
   try {
     const messages = await Message.find({ chat: req.params.chatId })
-      .populate("sender", "name pic email")
+      .populate("sender", "username avatar")
       .populate("chat");
     res.json(messages);
   } catch (error) {
@@ -35,11 +35,11 @@ const sendMessage = asyncHandler(async (req, res) => {
   try {
     var message = await Message.create(newMessage);
 
-    message = await message.populate("sender", "name pic").execPopulate();
-    message = await message.populate("chat").execPopulate();
+    message = await message.populate("sender", "username avatar");
+    message = await message.populate("chat");
     message = await User.populate(message, {
       path: "chat.users",
-      select: "name pic email",
+      select: "username avatar",
     });
 
     await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message });
