b565837cebb7948687f269b2684b13cb115482d3
Hennessy Nguyen <anguyen137@fordham.edu>
2024-05-02 22:05:55 -0400

    minor changes

--- utils/apis/spotify.js
+++ utils/apis/spotify.js
@@ -6,22 +6,6 @@ const getAccessToken = async () => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + (new Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64'))
-      },
-      body: 'grant_type=client_credentials'
-    });
-    const data = await response.json();
-    return data.access_token
-    // The response will return an access token valid for 1 hour
-};
-
-spotifyRouter.post('/getAccessToken', async (req, res) => {
-    try {
-    // const accessToken = await getAccessToken();
-    const response = await fetch('https://accounts.spotify.com/api/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
         'Authorization': 'Basic ' + (Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64'))
       },
       body: 'grant_type=client_credentials'
@@ -29,11 +13,18 @@ spotifyRouter.post('/getAccessToken', async (req, res) => {
     const data = await response.json();
     if (response.ok) {
         const accessToken = data.access_token;
-        console.log('Access token retrieved in backend:', accessToken); // Consider removing or masking for production
-        res.json({ accessToken });
-      } else {
-        throw new Error(data.error || 'Failed to retrieve access token');
-      }
+        console.log('Access token retrieved in backend:', accessToken); 
+        return accessToken
+    } else {
+    throw new Error(data.error || 'Failed to retrieve access token');
+    }
+    // The response will return an access token valid for 1 hour
+};
+
+spotifyRouter.post('/getAccessToken', async (req, res) => {
+    try {
+    const accessToken = await getAccessToken();
+    res.json({ accessToken });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
