7f3392aa520b8307a91f961acb6525d93c08a3a9
Ethan Nguyen <gsthanhnguyen@gmail.com>
2024-05-17 18:06:31 -0700

    Added to retrieve All Audios and the most recent audio

--- utils/apis/cloudinary.js
+++ utils/apis/cloudinary.js
@@ -1,10 +1,14 @@
 const cloudinary = require('cloudinary').v2;
-          
-cloudinary.config({ 
-  cloud_name: 'di6doert4', 
-  api_key: '493556213944719', 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+
+cloudinary.config({
+  cloud_name: 'dufirricm',
+  api_key: '237374651823171',
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+// Folder path for Audio files
+const audioFolderPath = 'Audios';
+const fileType = 'mp3';
 
 // const uploadImage = async (req, res) => {
 //     const { public_id } = req.body;
@@ -17,22 +21,64 @@ cloudinary.config({
 // }
 
 const getCloudinaryImages = async (req, res) => {
-    try {
-        const folder = 'PeacePod';
-    
-        const result = await cloudinary.search
-          .expression(`folder:${folder}`)
-          .execute();
-    
-        // console.log("result.resources", result.resources)
-        // const publicIds = result.resources.map(resource => resource.public_id);
-        const urls = result.resources.map(resource => resource.url); // or secure_url
-    
-        res.json(urls);
-      } catch (error) {
-        console.error('Error retrieving images:', error);
-        res.status(500).json({ error: 'Internal server error' });
-      }
+  try {
+    const folder = 'PeacePod';
+
+    const result = await cloudinary.search
+      .expression(`folder:${folder}`)
+      .execute();
+
+    // console.log("result.resources", result.resources)
+    // const publicIds = result.resources.map(resource => resource.public_id);
+    const urls = result.resources.map(resource => resource.url); // or secure_url
+
+    res.json(urls);
+  } catch (error) {
+    console.error('Error retrieving images:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 }
 
-module.exports = { getCloudinaryImages }
+// Get all audio files from Cloudinary
+const getCloudinaryAudios = async (req, res) => {
+  try {
+    const folder = 'PeacePod/Audios';
+
+    const result = await cloudinary.search
+      .expression(`folder:${folder}`)
+      .execute();
+
+    // console.log("result.resources", result.resources)
+    // const publicIds = result.resources.map(resource => resource.public_id);
+    const urls = result.resources.map(resource => resource.url); // or secure_url
+
+    res.json(urls);
+  } catch (error) {
+    console.error('Error retrieving images:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+const getCloudinaryAudio = async (req, res) => {
+  try {
+    const folder = 'PeacePod/Audios';
+    const result = await cloudinary.search
+      .expression(`folder:${folder}`)
+      .execute();
+
+    // Sort the resources by created_at in descending order
+    const sortedResources = result.resources.sort(
+      (a, b) => new Date(b.created_at) - new Date(a.created_at)
+    );
+
+    // Get the URL of the latest uploaded file
+    const latestFileUrl = sortedResources[0].url;
+
+    res.json(latestFileUrl);
+  } catch (error) {
+    console.error('Error retrieving images:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+module.exports = { getCloudinaryImages, getCloudinaryAudios, getCloudinaryAudio }
