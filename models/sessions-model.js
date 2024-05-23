50ca3a29710b8d0868a770611fefd416706a242e
Nam Nguyen <nguy1053@msu.edu>
2024-05-23 15:54:47 +0700

    Update session model according to new design

--- models/sessions-model.js
+++ models/sessions-model.js
@@ -1,13 +1,9 @@
 const mongoose = require("mongoose");
 
 const sessionsSchema = new mongoose.Schema({
-  user: {
+  userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: true,
-  },
-  uploadedBackgrounds: {
-    type: [String],
     required: true,
   },
   lastBackground: {
