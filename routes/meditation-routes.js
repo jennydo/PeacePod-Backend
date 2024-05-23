b993c571135450ebac0b6690a2cc07b0b3f086bb
Nam Nguyen <nguy1053@msu.edu>
2024-05-23 15:55:25 +0700

    Use requireAuth middleware for meditation routes

--- routes/meditation-routes.js
+++ routes/meditation-routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const { createMeditationAudio, getAllAudio, getAudio, createSession, getUserSession, getUserLastSession } = require('../controllers/meditation-controller')
-
+const requireAuth = require('../middleware/requireAuth')
 
 const meditationRouter = express.Router()
+
+meditationRouter.use(requireAuth)
 
 /// get a meditation session
 //meditationRouter.post('/sessions', getSession)
