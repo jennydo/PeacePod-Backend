3bd8cc0ca5bef32e9dd3b2d87fb239eb9e5508da
Nam Nguyen <nguy1053@msu.edu>
2024-05-24 12:07:11 +0700

    Fix attribute name: user to userId of meditationAudioSchema

--- models/meditation-audio-model.js
+++ models/meditation-audio-model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const meditationAudioSchema = new mongoose.Schema({
-    user: {
+    userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
