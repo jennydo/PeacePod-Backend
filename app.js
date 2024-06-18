5d69b0160b528ac9dccdf72408d01735978d6280
Hennessy Nguyen <anguyen137@fordham.edu>
2024-06-18 08:54:23 +0700

    fix typo socket event

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ mongoose
         chat.users.forEach((user) => {
           if (user._id == newMessageRecieved.sender._id) return;
 
-          socket.in(user._id).emit("message recieved", newMessageRecieved);
+          socket.in(user._id).emit("message received", newMessageRecieved);
         });
       });
 
